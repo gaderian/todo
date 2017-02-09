301223e8ef3417ffff53b36ea5bf58ea82ef83ef
erikm <erikm@cs.umu.se>
2017-02-09 15:00:33 +0100

    Some cleanup and comments

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 use std::error::Error;
 use std::fs::{File,OpenOptions};
 use std::io::prelude::*;
-use std::io::{BufReader,BufWriter};
+use std::io::BufReader;
 use std::env;
 use std::process;
 use std::fmt;
 
-struct a_line(u32,String);
+struct ALine(u32,String);
 
-impl fmt::Display for a_line {
+impl fmt::Display for ALine {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}: {}", self.0, self.1)
     }
 }
 
 fn main () {
-    // Create a path to the desired file
-
     let mut args = env::args();
     match args.next() {
         None => println!("I don't think its possible to get here"),
@@ -33,21 +31,20 @@ fn main () {
         "add" => add_entry(args),
         _ => println!("Not implemented"),
     };
-
-
 }
 
+/// Prints the lines in the todo file sorted and with their linenumber.
 fn list_entries() {
     let file = read_file();
-    let mut lines: Vec<String> = BufReader::new(file).lines()
+    let lines: Vec<String> = BufReader::new(file).lines()
         .map(|x| x.unwrap())
         .collect();
 
     let mut with_nr = {
-        let mut tmp: Vec<a_line> = Vec::new();
+        let mut tmp: Vec<ALine> = Vec::new();
         let mut i: u32 = 1;
         for line in lines {
-            tmp.push(a_line(i,line));
+            tmp.push(ALine(i,line));
             i=i+1;
         }
         tmp
@@ -59,20 +56,21 @@ fn list_entries() {
     }
 }
 
-fn add_entry(mut args: env::Args/*std::iter::Iterator<Item = String>*/) {
-    println!("not realy adding...");
-    let mut new_entry = String::new();
+/// Adds a new line to the todo file at the bottom with the specified entry
+fn add_entry(mut args: env::Args) {
     let mut writer = write_file();
+    let mut counter = 0;
+
     while let Some(mut word) = args.next() {
-        println!("{}", word);
         word.push('\n');
         writer.write_all(word.as_bytes());
+        counter+=1;
     }
-    writer.flush();
-    println!("{}", new_entry);
-
+    //writer.flush();
+    println!("Added {} new entrie(s).", counter);
 }
 
+/// Returns a file opened for appending
 fn write_file() -> File {
     let mut option = OpenOptions::new();
     option.write(true);
@@ -81,12 +79,14 @@ fn write_file() -> File {
     get_file(option)
 }
 
+/// Returns a file opened for reading
 fn read_file() -> File {
     let mut option = OpenOptions::new();
     option.read(true);
     get_file(option)
 }
 
+/// Opens a file with options set in the passed std::fs::OpenOptions
 fn get_file(option: OpenOptions) -> File {
     let home = match env::home_dir() {
         Some(path) => path,
@@ -98,10 +98,7 @@ fn get_file(option: OpenOptions) -> File {
     let path = tmp1.as_path();
     let display = path.display();
 
-    // Open the path in read-only mode, returns `io::Result<File>`
     let file = match option.open(&path) {
-        // The `description` method of `io::Error` returns a string that
-        // describes the error
         Err(why) => panic!("couldn't open {}: {}", display,
                            why.description()),
         Ok(file) => file,
